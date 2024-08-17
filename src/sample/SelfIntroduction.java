42dc2c144ba448337a8a86ddc3ef6e44ea9a2dde
自分 <@pc>
2024-08-18 07:55:39 +0900

    sampleプロジェクト作成

---
+++ src/sample/SelfIntroduction.java
@@ -0,0 +1,28 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		// 名前
+		String name = "八重倉　寛太";
+		// 年齢
+		int age = 29;
+		// 趣味
+		String hobby = "ゲーム";
+		
+		
+		/*
+		 * 自己紹介出力する処理
+		 */
+		
+		// 名前
+		System.out.println("私の名前は" + name + "です。");
+		// 年齢
+		System.out.println("年齢は" + age + "歳です。");
+		// 趣味
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
