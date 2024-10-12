48a52c99a6668b2abdaf10e09250e284bc406b7f
自分 <@pc>
2024-10-13 05:05:16 +0900

    1.31

--- src/main/Person.java
+++ src/main/Person.java
@@ -6,8 +6,8 @@ public class Person {
 	public double height;
 	public double weight;
 	
-    // 問題1: クラスフィールドの定義
-    public static int count = 0;
+	// 問題1: クラスフィールドの定義
+	public static int count = 0;
 
 	public	Person(String name, int age, double height, double weight){
 		this.name = name;
@@ -15,8 +15,8 @@ public class Person {
 		this.height = height;
 		this.weight = weight;
 		
-        // 問題2: countを1増やす
-        count++;
+		// 問題2: countを1増やす
+		count++;
 	}
 
 	public double bmi(){
@@ -28,10 +28,10 @@ public class Person {
 		System.out.println("年は" + this.age + "です");
 	}
 		
-	    // 問題4: クラスメソッドの定義
-    public static void printCount() {
-    	
-        // 問題5: クラスフィールドcountを用いて合計人数を表示
-        System.out.println("合計" + count + "人です");
+	// 問題4: クラスメソッドの定義
+	public static void printCount() {
+
+		// 問題5: クラスフィールドcountを用いて合計人数を表示
+		System.out.println("合計" + count + "人です");
 	}
 }
